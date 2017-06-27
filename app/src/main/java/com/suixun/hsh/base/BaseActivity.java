54aeecfb6364bf2b99ce22a6c97df1c7c5357e28
Tony <tony@gmail.com>
2017-06-27 20:02:08 +0800

    Merge branch 'master' of D:\MyApplication\suixun with conflicts.

--- app/src/main/java/com/suixun/hsh/base/BaseActivity.java
+++ app/src/main/java/com/suixun/hsh/base/BaseActivity.java
@@ -1,22 +1,15 @@
-package com.suixun.hsh.base;
+package com.suixun.hsh.ui.activity;
 
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class BaseActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
-
-
-
 }
---
+++ app/src/main/java/com/suixun/hsh/base/Contents.java
@@ -0,0 +1,8 @@
+package com.suixun.hsh.base;
+
+/**
+ * Created by Love—Sun on 2017/6/27.
+ */
+
+public class Contents {
+}
